81dcde0a314b264df541fc967e6ab0f8a11f5b40
arvid-e <arvid.edvinsson@gmail.com>
2026-09-19 13:58:10 +0900

    Implement quick sort algorithm

---
+++ part2/sorting_algorithms/quick_sort/algorithm.py
@@ -0,0 +1,23 @@
+def quick_sort(lst):
+    lst = lst[:]
+    if (len(lst) < 2):
+        return lst
+
+    pivot = len(lst) - 1
+    left = -1
+    right = 0
+
+    for right in range(pivot):
+        if (lst[right] < lst[pivot]):
+            helper = lst[left]
+            lst[left] = lst[right]
+            lst[right] = helper
+
+    helper = lst[left + 1]
+    lst[left + 1] = lst[pivot]
+    lst[pivot] = helper
+
+    sortedFirstHalf = quick_sort(lst[0: left + 1])
+    sortedSecondHalf = quick_sort(lst[left + 2: len(lst)])
+
+    return sortedFirstHalf + [lst[left + 1]] + sortedSecondHalf
